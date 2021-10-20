28df2640670cf27c97294a0820da07c97372812e
MasudSultan4 <mdsarwarjahanmasudsultan1971@gmail.com>
2021-10-20 19:10:05 +0600

    not found add

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 code-Link: https://github.com/Programming-Hero-Web-Course3/healthcare-related-website-MasudSultan4
 live-Site: https://sultan-hospital-da605.web.app
 
-#This is sultan Hospital
+# This is sultan Hospital
 
-##
+## this website show hospital services and doctors and vision etc.
+
+### and website click services deatails you are not register or login ,first register or login before see services details.
+
+####
--- src/compunent/NotFound/NotFound.js
+++ src/compunent/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
         <div>
-            <h1>404,Page is Not Found</h1>
-        </div>
+        <div className='error'></div>
+        <Link to="/home" className="btn btn-md btn-warning mt-3"><Button>Back to Home</Button></Link>
+    </div>
     );
 };
 
